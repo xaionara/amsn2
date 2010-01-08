831a5214683fd5eb65a4c790bf027030aa93afed
Boris 'billiob' Faure <billiob@gmail.com>
2010-01-08 21:59:53 +0100

    efl: WIP on CW

--- amsn2/ui/front_ends/efl/chat_window.py
+++ amsn2/ui/front_ends/efl/chat_window.py
@@ -48,11 +48,13 @@ class aMSNChatWidget(elementary.Box, base.aMSNChatWidget):
         self.outsc.size_hint_align_set(evas.EVAS_HINT_FILL,
                                        evas.EVAS_HINT_FILL)
         self.outsc.policy_set(elementary.ELM_SCROLLER_POLICY_AUTO,
-                              elementary.ELM_SCROLLER_POLICY_AUTO)
+                              elementary.ELM_SCROLLER_POLICY_ON)
         self.outsc.bounce_set(False, True)
         self.pack_end(self.outsc)
 
         self.outbx = elementary.Box(parent)
+        self.outbx.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+        self.outbx.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
         self.outsc.content_set(self.outbx)
         self.outbx.show()
         self.outsc.show()
@@ -76,6 +78,7 @@ class aMSNChatWidget(elementary.Box, base.aMSNChatWidget):
         self.ine.size_hint_weight_set(evas.EVAS_HINT_EXPAND,
                                       evas.EVAS_HINT_EXPAND)
         self.ine.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
+        self.ine.event_callback_add(evas.EVAS_CALLBACK_KEY_DOWN, self.__ine_key_down);
         self.insc.content_set(self.ine)
         self.ine.show()
         self.insc.show()
@@ -89,22 +92,54 @@ class aMSNChatWidget(elementary.Box, base.aMSNChatWidget):
 
         self.show()
 
-    def __sendButton_cb(self, button, entry):
-        pass
-        """
-        msg = self.__input_tb.text_get(0)
-        self.__input_tb.clear()
+    def __ine_key_down(self, obj, keydown):
+        if (keydown.keyname == "Return"):
+            ctrl = keydown.modifier_is_set("Control")
+            alt = keydown.modifier_is_set("Alt")
+            shift = keydown.modifier_is_set("Shift")
+            win = keydown.modifier_is_set("Super") or keydown.modifier_is_set("Hyper")
+            if (not ctrl and not alt and not shift and not win):
+                #TODO: Remove \n
+                str = obj.entry_get()
+                obj.entry_set("")
+                self.__send_msg(str)
+        elif (keydown.keyname == "Up"):
+            ctrl = keydown.modifier_is_set("Control")
+            alt = keydown.modifier_is_set("Alt")
+            shift = keydown.modifier_is_set("Shift")
+            win = keydown.modifier_is_set("Super") or keydown.modifier_is_set("Hyper")
+            if (ctrl and not alt and not shift and not win):
+                print "TODO: UP => prev msg"
+        elif (keydown.keyname == "Down"):
+            ctrl = keydown.modifier_is_set("Control")
+            alt = keydown.modifier_is_set("Alt")
+            shift = keydown.modifier_is_set("Shift")
+            win = keydown.modifier_is_set("Super") or keydown.modifier_is_set("Hyper")
+            if (ctrl and not alt and not shift and not win):
+                print "TODO: DOWN => prev msg"
+
+    def __sendMsg(self, msg):
+        bb = elementary.Bubble(self.parent)
+        bb.label_set("TODO: MYSELF")
+        bb.info_set("TODO: TIMESTAMP")
+        #TODO: bb.icon_set()
+        bb.size_hint_weight_set(evas.EVAS_HINT_EXPAND, 0.0)
+        bb.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
+        ab = elementary.AnchorBlock(self.parent)
+        ab.text_set(msg)
+        bb.content_set(ab)
+        ab.show()
+        self.outbx.pack_end(bb)
+        bb.show()
+
         strv = StringView()
         strv.appendText(msg)
         self._amsn_conversation.sendMessage(strv)
-        """
 
-    def __outputAppendMsg(self, msg):
-        pass
-        """
-        self.__output_tb.insert(self.__iter_out, msg)
-        """
-
+    def __sendButton_cb(button, entry):
+        str = entry.entry_get()
+        entry.entry_set("")
+        self.__sendMsg(msg)
 
     def onUserJoined(self, contact):
         print "%s joined the conversation" % (contact,)
@@ -116,10 +151,19 @@ class aMSNChatWidget(elementary.Box, base.aMSNChatWidget):
         print "%s is typing" % (contact,)
 
     def onMessageReceived(self, messageview, formatting=None):
-        pass
-        """
-        self.__outputAppendMsg(str(messageview.toStringView()))
-        """
+        print "MSG RECEIVED"
+        bb = elementary.Bubble(self.parent)
+        bb.label_set("TODO: CONTACT")
+        bb.info_set("TODO: TIMESTAMP")
+        #TODO: bb.icon_set()
+        bb.size_hint_weight_set(evas.EVAS_HINT_EXPAND, 0.0)
+        bb.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
+        ab = elementary.AnchorBlock(self.parent)
+        ab.text_set(str(messageview.toStringView()))
+        bb.content_set(ab)
+        ab.show()
+        self.outbx.pack_end(bb)
+        bb.show()
 
     def nudge(self):
         #TODO
