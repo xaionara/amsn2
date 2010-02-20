33534f4a0d20b53925e9e4ce666527fa7c7d15b8
Boris 'billiob' Faure <billiob@gmail.com>
2010-02-20 22:42:55 +0100

    web: turn web module into _web

--- amsn2/ui/front_ends/web/__init__.py
+++ amsn2/ui/front_ends/web/__init__.py
@@ -8,11 +8,12 @@ import sys
 def load():
     try:
         import web
-    except ImportError, e:
-        etype, value, tb = sys.exc_info()
-        traceback.print_exception(etype, value, tb.tb_next)
+        import amsn2.ui.front_ends.web._web
+    except ImportError:
+        etype, value, traceback = sys.exc_info()
+        traceback.print_exception(etype, value, traceback.tb_next)
         return None
-    return web
+    return amsn2.ui.front_ends.web._web
 
 # Initialize the front end by checking for any
 # dependency then register it to the guimanager
