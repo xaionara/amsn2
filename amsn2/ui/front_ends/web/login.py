df2d51dc255cec9b32eecca939ebc34b74f73dbc
Boris 'billiob' Faure <billiob@gmail.com>
2010-07-10 17:09:41 +0200

    web: start rewrite on login.py

--- amsn2/ui/front_ends/web/login.py
+++ amsn2/ui/front_ends/web/login.py
@@ -1,26 +1,23 @@
 class aMSNLoginWindow(object):
-    def __init__(self, amsn_core, main):
+    def __init__(self, core, main):
         self._main = main
-        self._amsn_core = amsn_core
+        self._core = core
 
     def __del__(self):
-        return
-        self._main.del_listener("signin", self.signin)
+        self._main.login_window = None
 
     def show(self):
-        return
-        self._main.add_listener("signin", self.signin)
+        self._main.login_window = self
 
     def hide(self):
-        return
-        self._main.send("hideLogin");
-        self._main.del_listener("signin", self.signin)
+        self._main.login_window = None
 
     def set_accounts(self, accountviews):
-        return
+        #TODO
+        pass
 
     def signin(self, u, p, *args, **kwargs):
-        accv = self._amsn_core._ui_manager.get_accountview_from_email(u)
+        accv = self._core._ui_manager.get_accountview_from_email(u)
         accv.password = password
 
         accv.presence = self.presence_key
@@ -30,9 +27,7 @@ class aMSNLoginWindow(object):
         accv.autologin = False
 
         logging.error("signing in")
-        self._amsn_core.quit()
-        self._amsn_core.signin_to_account(self, accv)
+        self._core.signin_to_account(self, accv)
 
     def on_connecting(self, progress, msg):
-        return
         self._main.send("onConnecting", msg)
